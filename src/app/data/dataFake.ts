4a85b54d5decba96b053fc90c10d8c9e6506712a
Allan <allan.mendes0976@gmail.com>
2024-12-20 20:50:29 -0300

    create dinamics components

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,32 @@
 export const dataFake = [
   {
     id: 1,
-    photo: 'AAA',
+    title: 'Novo filme da Marvel em 3D',
+    description: 'Saiba aonde assistir os Eternos.',
+    photoCover:
+      'https://rollingstone.com.br/media/_versions/2024/11/deadpool-wolverine-com-ryan-reynolds-e-hugh-jackman-estreia-no-disney_widelg.jpg',
+  },
+  {
+    id: 2,
     title: 'Notícias',
     description: 'lorem ipsu',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpq9lnVpPwIWVfJn6V76-DT1xsCjn216qtPA&s',
   },
 
   {
-    id: 2,
-    photo: 'BBB',
+    id: 3,
     title: 'Notícias do Brasil e do Mundo',
     description: 'lorem ipsu',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr_klv-asWUMRcvQV-8_Iok00x9qMHJhpaew&s',
+  },
+
+  {
+    id: 4,
+    title: 'Novo filme da Marvel em 3D',
+    description: 'Saiba aonde assistir os Eternos.',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgrdxcEp_kmZQoVueBrzPKwdTGWc7iVhoXhA&s',
   },
 ];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -6,12 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  photoCover: string =
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQagac0pNKYHOWwVIBFhzBwy-xmwpFbFrfVyQ&s';
-  contentTitle: string = 'Notícias';
-  contentDescription: string =
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro doloruminventore omnis lab';
-  constructor() {}
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
 
-  ngOnInit(): void {}
+  private id: string | null = '0';
+  constructor(private route: ActivatedRoute) {} //necessário para pegar os ids
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => {
+      this.id = value.get('id'); //pegando o valor do id do que estão vindo de outros components
+    });
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter(
+      (article) => article.id.toString() === id
+    )[0];
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photoCover;
+  }
 }
